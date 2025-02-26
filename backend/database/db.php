283470a87ed08e113401da6967df783aec4d453c
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-02-26 12:30:01 +0200

    add database

---
+++ backend/database/db.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "resturant";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function connect() {
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", 
+                                  $this->username, $this->password, [
+                                      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                                  ]);
+        } catch (PDOException $e) {
+            die("connection failed " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
+?>
