1b2ead06f6b37f4cd583f0af638e445118ae9531
Ahmed-A-Hegazy <v.ahmed.hegazy@gmail.com>
2025-02-28 21:00:00 +0200

    Added a review.php and reviews.php files with both the back-end and front-end logic and structure for sending and viewing user reviews.

---
+++ forms/php/reviews.php
@@ -0,0 +1,31 @@
+<?php
+require_once('hegazy_module.php');
+$reviews=get_reviews();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<head>
+    <link rel="stylesheet" href="/forms/styles/style.css">
+</head>
+    <body>
+        <form class='hegazy-form' style='font-size:200%'>
+            <div class="c">
+                <h1>Latest Reviews</h1>
+                <hr>
+
+                <?php
+                    foreach($reviews as $key=>$review){
+                        echo "<h4>".$review['firstName'].' '.$review['lastName']."</h4>";
+                        echo "<input type='rating' readOnly stars=5 value=".$review['rating']." style='color:rgb(255, 100, 0);'>";
+                        echo "<span>".$review['review']."</span>";
+                    }
+
+                ?>
+            </div>
+        </form>
+        <script>initialize()</script>
+
+    </body>
+</html>
