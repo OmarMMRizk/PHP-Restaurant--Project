ad38ea18c7f0785766ac46ef862f9a533031d958
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-03-01 23:31:39 +0200

    redesign dashboard

---
+++ views/sidebar.php
@@ -0,0 +1,11 @@
+<div class="sidebar">
+        <h4 class="text-center"><?=($_SESSION['user']['first_name']) ?> <?= ($_SESSION['user']['last_name'])?></h4>
+        <a href="../index.php"><i class="fa fa-home"></i> Home</a>
+        <a href="../products/products.php"><i class="fa fa-box"></i> Products</a>
+        <a href="categories.php"><i class="fa fa-th-list"></i> Categories</a>
+        <a href="../suppliers/suppliers.php"><i class="fa fa-truck"></i> Suppliers</a>
+        <a href="../reservations/reservationsList.php"><i class="fa fa-calendar-check"></i> Reservation</a>
+        <a href="profile.php"><i class="fa fa-user"></i> Profile</a>
+        <a href="manageuser.php"><i class="fa fa-user"></i> users</a>
+
+    </div>
